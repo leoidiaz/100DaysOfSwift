0fc94f8b932ae88206bbc77026764b517ceba237
leoidiaz <zaid.leonardo@gmail.com>
2020-03-28 14:55:08 -0700

    .DS_Store purge

--- Project7/Project7/DetailViewController.swift
+++ Project7/Project7/DetailViewController.swift
@@ -7,24 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
-
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+            <head>
+                <meta name = "viewport" content="width=device-width, initial-scale=1">
+                <style> body { font-size: 125%; font-family: Arial, YHelvetica, sans-serif; } p {line-height: 1.3;} </style>
+            </head>
+            <body>
+                <article>
+                    <h1>\(detailItem.title)</h1>
+                    <p>\(detailItem.body)</p>
+                </article>
+            </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -14,39 +14,127 @@ class ViewController: UITableViewController {
 
     var petitions = [Petition]()
     
+    var filterPetitions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlSting = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        title = "White House Petitions"
+        
+        // Credits Button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .plain, target: self, action: #selector(displayCredits))
+        
+        // Search Button
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(displaySearchBar))
+    
+        let urlSting: String
+            
+        if navigationController?.tabBarItem.tag == 0 {
+            urlSting = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlSting = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
         
         if let url = URL(string: urlSting){
             if let data = try? Data(contentsOf: url){
                 parse(json: data)
+                return
             }
         }
+        showError()
     }
+    
+    //MARK: - Error Alert Controller
+    
+    func showError(){
+        let alertController = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
+    }
+    
+    //MARK: - JSON Parse
     
     func parse(json: Data){
         let decoder = JSONDecoder()
 
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
             petitions = jsonPetitions.results
+            filterPetitions = jsonPetitions.results
             tableView.reloadData()
         }
+    }
+    
+    //MARK: - Credit Alert Controller
+    
+    @objc func displayCredits(){
+        let alertController = UIAlertController(title: "Credits", message: "We The People API of the Whitehouse", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Dismiss", style: .cancel)
+        alertController.addAction(dismiss)
+        present(alertController, animated: true)
+    }
+    
+    //MARK: - Search Alert Controller
+    
+    @objc func displaySearchBar(){
+        let alertController = UIAlertController(title: "Search", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        // Search Action
+        let searchAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak alertController] _ in
+            guard let searchQuery = alertController?.textFields?[0].text else { return }
+            self?.filterSearch(query: searchQuery)
+        }
+        
+        // Reset Action
+        let reset = UIAlertAction(title: "Reset", style: .default) { [weak self] _ in
+            self?.title = "White House Petitions"
+            self?.filterPetitions = self?.petitions ?? [Petition]()
+            self?.tableView.reloadData()
+        }
+        
+        alertController.addAction(reset)
+        alertController.addAction(searchAction)
+        present(alertController, animated: true)
+    }
+    
+    // Filter results
+    func filterSearch(query: String){
+        filterPetitions.removeAll()
+        
+        let lowercaseQuery = query.lowercased()
+        
+        if query.isEmpty {
+            filterPetitions = petitions
+            title = "White House Petitions"
+        } else {
+            for petition in petitions {
+                if petition.title.lowercased().contains(lowercaseQuery) || petition.body.lowercased().contains(lowercaseQuery) {
+                    filterPetitions.append(petition)
+                }
+            }
+            title = "\(filterPetitions.count) results found"
+        }
+        tableView.reloadData()
     }
     
     //MARK: - TableView Delegates
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filterPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reusueIdentifier, for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filterPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = filterPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
     
 }
