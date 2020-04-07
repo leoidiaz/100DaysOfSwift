9f224a2369e2f590132f8fd46caf5775efd3dcc8
leoidiaz <zaid.leonardo@gmail.com>
2020-04-07 14:06:40 -0700

    Project 10 Person and Person Cell File

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Leonardo Diaz on 4/6/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
