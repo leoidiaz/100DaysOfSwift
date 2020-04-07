ea23e2928093aaae567cf92053dc66301af98cd4
leoidiaz <zaid.leonardo@gmail.com>
2020-04-07 14:08:45 -0700

    Project 10 Challenge 1 and 2

--- Project10/Project10/Person.swift
+++ Project10/Project10/Person.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class Person: NSObject {
-
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
 }
--- Project10/Project10/PersonCell.swift
+++ Project10/Project10/PersonCell.swift
@@ -9,5 +9,9 @@
 import UIKit
 
 class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
     
 }
