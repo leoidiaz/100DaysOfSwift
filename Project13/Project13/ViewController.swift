d2b1b2ae0a48096e8943ab847f69b0ee389284b0
leoidiaz <zaid.leonardo@gmail.com>
2020-05-24 12:02:22 -0700

    Project 13 part one complete

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensitySlider: UISlider!
+    
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Instafiler"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add
+            , target: self, action: #selector(importPicture))
     }
+    
+    @objc func importPicture(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @IBAction func changeFilterTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func intensitySliderChanged(_ sender: Any) {
+    }
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
 }
 
