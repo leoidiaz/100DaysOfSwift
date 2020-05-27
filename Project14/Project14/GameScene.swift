59c47d472d7801711102a3e8ecdab616fc971b78
leoidiaz <zaid.leonardo@gmail.com>
2020-05-27 10:36:30 -0700

    Project 14 Start

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Leonardo Diaz on 5/27/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
