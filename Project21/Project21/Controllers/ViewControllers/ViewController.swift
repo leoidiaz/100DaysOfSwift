9763f58509da16aaec816a16b81966a9bd736362
leoidiaz <zaid.leonardo@gmail.com>
2020-07-30 16:28:04 -0700

    Project 21 part 1

---
+++ Project21/Project21/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by Leonardo Diaz on 7/30/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UserNotifications
+import UIKit
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+      navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+
+    }
+
+    @objc func registerLocal(){
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+        }
+    }
+    
+    @objc func scheduleLocal(){
+        registerCategories()
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+
+        let content = UNMutableNotificationContent()
+        
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the mouse."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData" : "fizzbuzz"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    
+    func registerCategories(){
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data receieved: \(customData)")
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                print("Default identifier")
+            case "show":
+                print("Show more information...")
+            default:
+                break
+            }
+        }
+        completionHandler()
+    }
+}
